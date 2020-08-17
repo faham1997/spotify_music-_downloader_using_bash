84ec56785533c23befb66a7e69e696b0487727ab
faham1997 <nafidfaham08@gmail.com>
2020-08-17 08:29:29 +0600

    spotifydl: add condition to check if spotdl module is installed or not if spotdl is not installed the user  can install it through the script or cancel it as well.

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#Check if spotdl module is installed or not
+if ! command -v spotdl &> /dev/null
+then
+    echo "spotdl is not installed. "
+    
+    # Prompt to install spotdl
+    echo "Do you want to install spotdl? "
+    read ans
+    if [[ "$ans" == "yes" ]];then
+        pip3 install spotdl
+        echo "spotdl sucessfully installed!"
+    else
+        exit 127
+    fi
+fi
+
 echo "Enter the song url you want to download : "
 read url
 
