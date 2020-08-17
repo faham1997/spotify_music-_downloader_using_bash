96ab219b384e3bd6b1c05cb647110a95c082ef23
faham1997 <nafidfaham08@gmail.com>
2020-08-18 03:21:54 +0600

    spotifydl: added ability to download album/playlist/songs

--- run.sh
+++ run.sh
@@ -16,7 +16,41 @@ then
     fi
 fi
 
-echo "Enter the song url you want to download : "
-read url
+#Need the condition of checking if ffmpeg is isntalled or not
 
-spotdl --song $url
+#Implement a condition to choose between songs/playlist/album
+echo "Specify your choice :
+    Press 1 for downloading a song
+    Press 2 for downloading a playlist
+    Press 3 for downloading an album"
+read arg
+
+if [[ "$arg" == "1" ]];then
+    echo "You have selected to download song"
+    arg="song"
+
+    echo "Enter the $arg url you want to download : "
+    read url
+    spotdl --$arg $url
+
+elif [[ "$arg" == "2" ]];then
+    echo "You have selected to download playlist"
+    arg="playlist"
+
+    echo "Enter the $arg url you want to download : "
+    read url
+    spotdl --$arg $url --write-to temp.txt
+    spotdl --list=temp.txt
+
+elif [[ "$arg" == "3" ]];then
+    echo "You have selected to download album"
+    arg="album"
+    
+    echo "Enter the $arg url you want to download : "
+    read url
+    spotdl --$arg $url --write-to temp.txt
+    spotdl --list=temp.txt
+else
+    echo "Invalid Selection"
+    exit 127
+fi
